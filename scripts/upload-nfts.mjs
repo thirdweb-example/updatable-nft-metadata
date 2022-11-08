2072837524cac0b0eb5e6f513e503fca02ddda81
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-11-08 09:19:33 +0530

    feat: init

---
+++ scripts/upload-nfts.mjs
@@ -0,0 +1,25 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import { config } from "dotenv";
+
+config();
+
+const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "mumbai");
+
+const addNFTs = async () => {
+  const drop = await sdk.getContract(
+    "0x40739224a8818C381acaeAe2c620eeD84fEd35F2",
+    "nft-drop"
+  );
+
+  try {
+    await drop.createBatch([
+      "https://my-json-server.typicode.com/avneesh0612/updatable-metadata/nfts/0",
+      "https://my-json-server.typicode.com/avneesh0612/updatable-metadata/nfts/1",
+    ]);
+    console.log("uploaded all nfts");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+addNFTs();
